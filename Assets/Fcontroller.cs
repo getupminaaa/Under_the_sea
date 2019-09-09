69f7e421af451f4e5558d7bbacfb7e68036fa934
getupminaaa <54892112+getupminaaa@users.noreply.github.com>
2019-09-09 17:51:30 +0900

    언더더씨 게임파일들
    물고깅

---
+++ Assets/Fcontroller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fcontroller : MonoBehaviour
+{
+
+    Rigidbody2D rb2d;
+    bool isDead;
+
+    public float maxHeight;
+    public float flapVelocity;
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && transform.position.y < maxHeight) { Flap(); }
+
+    }
+    public void Flap()
+    {
+        if (isDead) return;
+        if (rb2d.isKinematic) return;
+        rb2d.velocity = new Vector2(0.0f, flapVelocity);
+    }
+  
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isDead) return;
+        Camera.main.SendMessage("Clash");
+        isDead = true;
+    }
+    public void SetSteerActive(bool active)
+    {
+        rb2d.isKinematic = !active;
+    }
+}
---
+++ Assets/Trigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    GameObject gameConroller;
+    void Start()
+    {
+        gameConroller = GameObject.FindWithTag("GameController");
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        gameConroller.SendMessage("IncreaseScore");
+    }
+}
