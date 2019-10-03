82a627e65e455e0e917dd43cf94fefd91741f241
getupminaaa <54892112+getupminaaa@users.noreply.github.com>
2019-10-03 15:13:34 +0900

    배경 바꾼것
    fade out- in 효과 추가

---
+++ Assets/FadeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeController : MonoBehaviour
+{
+    int time = 3;
+    SpriteRenderer sr;
+    private bool fade;
+
+    private void Start()
+    {
+        sr = this.gameObject.GetComponent<SpriteRenderer>();
+        fade = true;
+    }
+
+    private void Update()
+    {
+        Color tempColor = sr.color;
+        if (!fade)
+        {
+            tempColor.a -= Time.deltaTime / time;
+            sr.color = tempColor;
+
+            if (tempColor.a <= 0f)
+            {
+                tempColor.a = 0f;
+                fade = true;
+            }
+        }
+        else
+        {
+            tempColor.a += Time.deltaTime / time;
+            sr.color = tempColor;
+
+            if (tempColor.a >= 1f)
+            {
+                tempColor.a = 1f;
+                fade = false;
+            }
+        }
+
+        sr.color = tempColor;
+    }
+}
+
+
