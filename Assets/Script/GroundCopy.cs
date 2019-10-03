9dfc51683603cf07f128071deb6c3f30a8ce9a91
hurjeesic <hurjeesic@naver.com>
2019-10-03 20:28:39 +0900

    닻줄 추락 Ground 자동 증식

---
+++ Assets/Script/GroundCopy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GroundCopy : MonoBehaviour
+{
+    float size = 2.7f;
+    public GameObject originGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float pos = originGround.transform.position.x;
+        for (int i = 1; i <= 30; i++)
+        {
+            pos += size;
+            GameObject ground = Instantiate(originGround, gameObject.transform);
+            ground.transform.position = new Vector2(pos, ground.transform.position.y);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
