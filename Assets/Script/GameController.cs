7843d46247e08ae2022ae4cdb426c3fdb5ff2091
hurjeesic <hurjeesic@naver.com>
2019-10-03 19:03:43 +0900

    점수 보여주기

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    public Fcontroller character;
+    GUIStyle guiStyle;
+
     private void Start()
     {
-
+        guiStyle = new GUIStyle("Button");
+        guiStyle.alignment = TextAnchor.MiddleCenter;
     }
 
     private void Update()
     {
-        
+       if (character.IsDead() && Input.GetKey(KeyCode.Space))
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+
+    private void OnGUI()
+    {
+        string text = "점수 : " + character.Score + "점";
+        GUI.Label(new Rect(30, 10, text.Length * 10, 30), text, guiStyle);
+        if (character.IsDead() && GUI.Button(new Rect(Screen.width / 2 - 75, Screen.height / 2 - 25, 150, 50), "다시 시작(Space)"))
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene("SampleScene");
+        }
     }
 
     public void RecordRank(int score)
