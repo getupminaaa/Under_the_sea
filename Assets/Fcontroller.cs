9f7ae3088b2a4a4973557aff59bfaf3ab260160d
hurjeesic <hurjeesic@naver.com>
2019-10-03 16:49:55 +0900

    점수 기록 구현

--- Assets/Fcontroller.cs
+++ Assets/Fcontroller.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Fcontroller : MonoBehaviour
 {
+    public GameController controller;
+
+    [HideInInspector]
+    public int score;
 
     Rigidbody2D rb2d;
     bool isDead;
@@ -20,39 +22,57 @@ public class Fcontroller : MonoBehaviour
     void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        
+        Initalize();
+    }
+
+    public void Initalize()
+    {
+        score = 0;
+        isDead = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isDead) return;
-        if (Input.GetKey(KeyCode.UpArrow) == true) {
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
             rb2d.AddForce(transform.up * thrust * Time.deltaTime);
             Debug.Log("UpArrow");
-        } else if (Input.GetKey(KeyCode.DownArrow) == true)
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) == true)
         {
-            rb2d.AddForce(transform.up* -1 * thrust * Time.deltaTime);
+            rb2d.AddForce(transform.up * -1 * thrust * Time.deltaTime);
             Debug.Log("DownArrow");
-        } else if (Input.GetKey(KeyCode.RightArrow) == true)
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) == true)
         {
             Debug.Log("RightArrow");
             rb2d.AddForce(transform.right * thrust * Time.deltaTime);
-        } else if(Input.GetKey(KeyCode.LeftArrow) == true)
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) == true)
         {
-            rb2d.AddForce(transform.right* -1 * thrust * Time.deltaTime);
+            rb2d.AddForce(transform.right * -1 * thrust * Time.deltaTime);
             Debug.Log("LeftArrow");
         }
-
-
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (isDead) return;
+        if (collision.collider.tag == "Item")
+        {
+            score++;
+        }
+        else
+        {
+            isDead = true;
+            controller.RecordRank(score);
+            Time.timeScale = 0;
+        }
+        
         //Camera.main.SendMessage("Clash");
-        isDead = true;
     }
+
     public void SetSteerActive(bool active)
     {
         rb2d.isKinematic = !active;
--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void RecordRank(int score)
+    {
+        Debug.Log("점수는 " + score + "입니다.");
+        for (int i = 1; i <= 10; i++)
+        {
+            int rankScore = PlayerPrefs.GetInt(i + "위", -1);
+            if (score > rankScore)
+            {
+                Debug.Log(i + "위를 기록하였습니다.");
+                for (int j = 10; j > i; j--)
+                {
+                    PlayerPrefs.SetInt(j + "위", PlayerPrefs.GetInt((j - 1) + "위", -1));
+                }
+
+                PlayerPrefs.SetInt(i + "위", score);
+                break;
+            }
+        }
+
+        score = 0;
+    }
+}
