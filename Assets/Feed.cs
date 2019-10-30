3e1cc47f979d3bf0d6c8a2080dcc419b44ecbc8b
getupminaaa <54892112+getupminaaa@users.noreply.github.com>
2019-10-31 00:44:32 +0900

    미끼 추가
    각 미끼마다 스크롤, 재생성, 높이랜덤 스크립트 추가함

---
+++ Assets/Feed.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Feed : MonoBehaviour
+{
+    [HideInInspector]
+    public float speed;
+    [HideInInspector]
+    public float startPosition;
+    [HideInInspector]
+    public float endPosition;
+
+
+    public GameObject[] character;
+    private void OnTriggerEnter2D(Collider2D colFe)
+    {
+        if (colFe.gameObject.tag == "Player")
+        {
+            Start();
+            ScrollEnd();
+        }
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        speed = 4;
+        startPosition = 35;
+        endPosition = -startPosition;
+    }
+    void ScrollEnd()
+    {
+        transform.Translate(-1 * (endPosition - startPosition), 0, 0);
+        SendMessage("OnScrollEnd", SendMessageOptions.DontRequireReceiver);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
