c15ac9e1de0707c0a11273ebede2f35749491dcb
hurjeesic <hurjeesic@naver.com>
2019-11-01 02:41:08 +0900

    성게 원운동 추가

---
+++ Assets/Script/MoveCircle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class MoveCircle : MonoBehaviour
+{
+    public float speed = 3f;
+    public float radius = 0.1f;
+
+    private Vector2 center;
+    private float angle;
+
+    private void Start()
+    {
+        center = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Time.deltaTime != 0)
+        {
+            angle += speed * Time.deltaTime;
+
+            transform.Translate(new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * radius);
+        }
+    }
+}
